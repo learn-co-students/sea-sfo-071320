a9fa3ce43e1d7c0a995c095bff8eff12c7c3144f
Amelie Oller <amelieoller@gmail.com>
2020-08-06 12:23:22 -0700

    Create rails crud lecture

---
+++ 19-rails-crud/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/game/:slug', to: 'games#show'
+
+  resources :games
+
+  # post '/search', to: 'games#search'
+  # delete '/games/:id', to: 'games#destroy'
+end
