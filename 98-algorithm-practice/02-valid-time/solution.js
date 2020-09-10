f00e8b2db2d8fde53facc2eed36cb9a408852d90
hbj <heyhal19@gmail.com>
2020-09-10 13:26:50 -0700

    danira's solution:

--- 98-algorithm-practice/02-valid-time/solution.js
+++ 98-algorithm-practice/02-valid-time/solution.js
@@ -1,1 +1,20 @@
 // Danira
+
+const timeValid = (string) => {
+    if (string.length === 5) {
+        let s = string.split(':')
+        let front = +s[0]
+        let back = +s[1]
+        if(front > 0 && front < 24 && back <= 59 && back > 0){
+            return true
+        } else {
+            return false
+        }
+    }else {
+        return false
+    }
+}
+// let string = '13:58'
+// let string = '25:51'
+let string = '02:76'
+console.log(timeValid(string))
