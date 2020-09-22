87cc559be8f235f64a16fcd98b2c4138c4108883
emileypalmquist <emiley.palmquist@icloud.com>
2020-09-21 18:08:44 -0700

    added code for new todo

--- 40-react-review-to-do-list/src/App.js
+++ 40-react-review-to-do-list/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import TodosContainer from './containers/TodosContainer';
 // import CompletedToDosContainer from './containers/CompletedTodosContainer';
-// import NewTodoForm from './components/NewTodoForm';
+import NewTodoForm from './components/NewTodoForm';
 
 const ENDPOINT = 'http://localhost:6001/todos';
 
 class App extends Component {
   state = {
     todos: [],
+    newTodo: '',
   };
 
   componentDidMount() {
@@ -32,6 +33,7 @@ class App extends Component {
 
   patchCompleted = (todo) => {
     let data = { completed: !todo.completed };
+
     fetch(ENDPOINT + `/${todo.id}`, {
       method: 'PATCH',
       headers: {
@@ -42,8 +44,8 @@ class App extends Component {
   };
 
   deleteTask = (todo) => {
-    console.log(todo);
     const id = todo.id;
+
     this.setState((prevState) => ({
       todos: prevState.todos.filter((todo) => todo.id !== id),
     }));
@@ -51,12 +53,45 @@ class App extends Component {
     fetch(ENDPOINT + `/${id}`, { method: 'DELETE' });
   };
 
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTodo = this.state.newTodo;
+
+    const body = {
+      content: newTodo,
+      completed: false,
+    };
+
+    fetch(ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((newTask) =>
+        this.setState((prevState) => ({ todos: [...prevState.todos, newTask] }))
+      );
+
+    this.setState({ newTodo: '' });
+  };
+
+  changeHandler = (event) => {
+    this.setState({ newTodo: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
         <h1 className="title">To Do List</h1>
         <h4>Let's be Productive!</h4>
-        {/* <NewTodoForm /> */}
+        <NewTodoForm
+          changeHandler={this.changeHandler}
+          handleFormSubmit={this.handleFormSubmit}
+          newTodo={this.state.newTodo}
+        />
 
         <TodosContainer
           todos={this.state.todos}
--- 40-react-review-to-do-list/src/components/NewTodoForm.js
+++ 40-react-review-to-do-list/src/components/NewTodoForm.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
-const NewTodoForm = () => (
-  <form>
-    <input type="text" placeholder="Todo Content" />
+const NewTodoForm = (props) => (
+  <form onSubmit={props.handleFormSubmit}>
+    <input
+      onChange={props.changeHandler}
+      type="text"
+      placeholder="Todo Content"
+      value={props.newTodo}
+    />
     <input type="Submit" />
   </form>
 );
-
 export default NewTodoForm;
