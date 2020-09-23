4e1a02a96e3392a4b8cdbd954172d38135367e40
hbj <heyhal19@gmail.com>
2020-09-23 15:24:57 -0700

    some solutions

--- 98-algorithm-practice/06-longest-common-prefix/solution.js
+++ 98-algorithm-practice/06-longest-common-prefix/solution.js
@@ -3,3 +3,48 @@
 function longestCommonPrefix(arr){
 
 }
+
+
+// nathan
+function longestCommonPrefix(strs) {
+  if (!strs.length) return '';
+for(i=0; i < strs.length; i++) {
+   for (let str of strs) {
+     if (str[i] !== strs[0][i]) {return str.slice(0, i)}
+     }
+    }
+    return strs[0]
+};
+
+
+// Alec
+
+function longestCommonPrefix(arr) {
+    let prefix = '';
+    let go = true;
+    let matches = false;
+    for (let i = 0; i < arr[0].length; i++) {
+      if (!go) break;
+      matches = false;
+      for (const string of arr) {
+        if (string[i] !== arr[0][i]) {
+          matches = false;
+          go = false;
+          break;
+        } else {
+          matches = true;
+        }
+      }
+      if (matches) prefix += arr[0][i];
+    }
+    return prefix;
+  }
+  const test1 = longestCommonPrefix(['flower', 'flow', 'flight']);
+  // => "fl"
+  const test2 = longestCommonPrefix(['dog', 'racecar', 'car']);
+  // => ""
+  const test3 = longestCommonPrefix(['Hal', 'Halented', 'Hallucinogen']);
+  // => "Hal"
+  console.log(test1);
+  console.log(test2);
+  console.log(test3);
