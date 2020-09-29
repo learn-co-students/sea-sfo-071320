cea46292da3fcb96f540f0416b532471388d8b99
hbj <heyhal19@gmail.com>
2020-09-29 13:39:20 -0700

    solutions

--- 98-algorithm-practice/07-adjacent-elements/solution.js
+++ 98-algorithm-practice/07-adjacent-elements/solution.js
@@ -1,1 +1,13 @@
 // Lucas
+
+function adjacentElements(array) {
+    // parse through array, at each element multiply with next. compare with next pair, set larger const omparison to the max product.
+    let max = 0 
+    for(let i =0; i < array.length; i++) {
+       if (array[i] * array[i + 1] > max) {
+         max = array[i] * array[i + 1] 
+       }
+    }
+    return max 
+  }
+adjacentElements([9,9,9,9,9,1]);
